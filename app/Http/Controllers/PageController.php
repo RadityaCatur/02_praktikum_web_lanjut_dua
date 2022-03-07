6bfd55aa38e72af5783dabf601b5d4f192acc38e
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-03-07 10:55:41 +0700

    Menampilkan Nama dan NIM

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,4 +9,10 @@ class PageController extends Controller
     public function index() {
         return 'Selamat Datang!';
     }
+
+    
+    public function about() {
+        echo 'Nama: Raditya Catur Narendra<br>';
+        echo 'NIM: 2041720198';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\PageController@index');
+Route::get('/about', 'App\Http\Controllers\PageController@about');
